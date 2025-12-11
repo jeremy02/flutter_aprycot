6c8773f12e538dc00a4590ff41c97dc1832d554d
Jeremiah Thuku <thuku8@gmail.com>
2025-12-11 18:19:08 +0300

    done with the dashboard banner right banner

--- lib/screens/dashboard/dashboard_screen.dart
+++ lib/screens/dashboard/dashboard_screen.dart
@@ -3,29 +3,102 @@ import 'package:flutter/material.dart';
 class DashboardScreen extends StatelessWidget {
   const DashboardScreen({super.key});
 
+  final double circleRadius = 100.0;
+  final double circleBorderWidth = 8.0;
+
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
+      height: 320,
       decoration: BoxDecoration(
         color: Color(0xFFFFf5EE),
       ),
       child: Padding(
-          padding: EdgeInsets.fromLTRB(16, 16, 16, 16),
+          padding: EdgeInsets.all(20.0),
           child: Row(
             children: [
               Flexible(
                 flex: 63,
                 child: Container(
-                  color: Colors.blue,
-                  child: Center(child: Text('63%')),
+                  clipBehavior: Clip.antiAlias,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  child: Image.asset(
+                    'images/dashboard/banner_image.png',
+                    fit: BoxFit.cover,
+                    height: double.infinity,
+                    width: double.infinity,
+                  ),
                 ),
               ),
+              const SizedBox(width: 20),
               Flexible(
                 flex: 37,
-                child: Container(
-                  color: Colors.green,
-                  child: Center(child: Text('37%')),
+                child: Stack(
+                  alignment: Alignment.topCenter,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(
+                          top: 64,
+                      ),
+                      child: Container(
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          color: Colors.white.withOpacity(0.8),
+                          borderRadius: BorderRadius.circular(16),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.2),
+                              spreadRadius: 1,
+                              blurRadius: 2,
+                              offset: Offset(-0.7, 1.2),
+                            ),
+                          ],
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text(
+                              '50 % off',
+                              style: TextStyle(
+                                fontSize: 36,
+                                fontWeight: FontWeight.w800,
+                                color: Color(0xFF07143B),
+                                letterSpacing: -1,
+                              ),
+                            ),
+                            const SizedBox(height: 8),
+                            Text(
+                              'The full price of burgers',
+                              style: TextStyle(
+                                fontSize: 14,
+                                color: const Color(0xFF959895),
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                            const SizedBox(height: 16),
+                          ],
+                        ),
+                      ),
+                    ),
+                    LayoutBuilder(
+                      builder: (context, constraints) {
+                        final double imageSize = constraints.maxWidth.clamp(150, 260);
+
+                        return SizedBox(
+                          width: imageSize,
+                          height: imageSize * 0.75,
+                          child: Image.asset(
+                            'images/dashboard/burger_image.png',
+                            fit: BoxFit.cover,
+                          ),
+                        );
+                      },
+                    ),
+                  ],
                 ),
               ),
             ],
--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_sidebar_x/utils/responsive.dart';
 import 'package:get/get.dart';
 import '../../controllers/app_menu_controller.dart';
 import '../components/navigation/navigation_rail_widget.dart';
@@ -15,14 +16,18 @@ class MainScreen extends StatelessWidget {
       body: Row(
         children: [
           NavigationRailWidget(controller: controller),
-          Expanded(child: _buildMainContent()),
+          Expanded(
+              child: _buildMainContent(context),
+          ),
         ],
       ),
     );
   }
 
-  Widget _buildMainContent() {
+  Widget _buildMainContent(BuildContext context) {
     return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.center,
       children: [
         Container(
           padding: const EdgeInsets.all(16),
@@ -42,24 +47,25 @@ class MainScreen extends StatelessWidget {
                 style: TextStyle(
                     fontSize: 20,
                     fontWeight: FontWeight.bold,
-                    color: Colors.grey.shade800),
+                    color: Colors.grey.shade800
+                ),
               )),
             ],
           ),
         ),
         Expanded(
-          child: Center(
-            child: Obx(
-                  () {
-                if (controller.selectedSubmenuId.value == "dashboard") {
-                  // Only show this text when submenu is selected
-                  return SingleChildScrollView(
-                    child: DashboardScreen(),
-                  );
-                }
+          child: Obx(
+                () {
+              if (controller.selectedSubmenuId.value == "dashboard" && Responsive.isDesktop(context)) {
+                // Only show this text when submenu is selected
+                return SingleChildScrollView(
+                  child: DashboardScreen(),
+                );
+              }
 
-                // Otherwise show the full column
-                return Column(
+              // Otherwise show the full column
+              return Center(
+                child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Icon(
@@ -77,13 +83,13 @@ class MainScreen extends StatelessWidget {
                         )
                     ),
                     const SizedBox(height: 8),
-                    Text('Welcome to ${controller.selectedSubmenuId.value} section'),
+                    Text('Welcome to ${controller.currentPage.value} section'),
                   ],
-                );
-              },
-            ),
-          ),
-        )
+                ),
+              );
+            },
+          )
+        ),
       ],
     );
   }
